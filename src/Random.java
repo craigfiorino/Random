54111a51fd6a24449d98abdd4988e09300b543ad
Craig Fiorino <craigfiorino@billericak12.com>
2021-09-22 11:29:25 -0400

    sjcgsjagjsagjv

---
+++ src/Random.java
@@ -0,0 +1,9 @@
+public class Random {
+    public static void main(String[] args) {
+    double r = Math.random();
+    System.out.println(r);
+    r= Math.random() * 10;
+    System.out.println(r);
+    int p = (int) (Math.random() * 10);
+    System.out.println(p);
+}}
